c20b27090d9448f948569de51dab694da3e7b7e5
simantabarua <webdevelopersimanta@gmail.com>
2023-09-17 22:16:17 +0600

    slider added fix responsive

--- js/main.js
+++ js/main.js
@@ -12,3 +12,39 @@ function toggleNavbarTransparency() {
 window.addEventListener("scroll", toggleNavbarTransparency);
 
 toggleNavbarTransparency();
+
+// js for swiper:
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
+const mobileMenuButton = document.querySelector(
+  'button[aria-controls="mobile-menu"]'
+);
+const mobileMenu = document.querySelector("#mobile-menu");
+const openIcon = document.querySelector(".open-icon");
+const closeIcon = document.querySelector(".close-icon");
+
+// Add click event listener to the mobile menu button
+mobileMenuButton.addEventListener("click", () => {
+  const isExpanded = mobileMenuButton.getAttribute("aria-expanded") === "true";
+
+  // Toggle the visibility and position of the mobile menu
+  if (isExpanded) {
+    mobileMenu.style.transform = "translateX(-100%)";
+  } else {
+    mobileMenu.style.transform = "translateX(0%)";
+  }
+  mobileMenuButton.setAttribute("aria-expanded", !isExpanded);
+
+  // Toggle the visibility of the icons
+  openIcon.classList.toggle("hidden", !isExpanded);
+  closeIcon.classList.toggle("hidden", isExpanded);
+});
